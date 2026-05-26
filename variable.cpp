699faeb1a398810654e610254ad420073b33219f
Deepa Rajput <251839642+deeparajput-78@users.noreply.github.com>
2026-05-26 23:02:21 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x =  5;
+    // cout<<x<<endl;
+    //  x = 7;
+    // cout<<x<<endl;
+    // x +=7;
+    // cout<<x<<endl;
+    // x  -= 7;
+    // // 13-20=-7
+    // cout<<x<<endl;
+   int  x=7;
+//    cout<<x+y<<endl;
+//    cout<<x-y<<endl;
+//    cout<<x*y<<endl;
+//    cout<<x/y<<endl;
+    //    cout<<x<<endl;
+    //    ++x;//preincrement
+    //    cout<<x<<endl;
+    //    --x;//predecrement
+    //    cout<<x<<endl;
+    //    x++;
+    //    cout<<x<<endl;
+    //    x--;
+    //    cout<<x<<endl;
+       cout<<x<<endl;
+       cout<<x++<<endl;
+       cout<<x<<endl;
+
+
+       cout<<++x<<endl;
+       cout<<x<<endl;
+
+       cout<<x--<<endl;
+       cout<<x<<endl;
+       cout<<--x<<endl;
+       cout<<x<<endl;
+
+    return 0;
+}
