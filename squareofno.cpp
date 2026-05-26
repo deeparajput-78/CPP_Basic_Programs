198fd52e11953e68a46f538074dee08484e5cf69
Deepa Rajput <251839642+deeparajput-78@users.noreply.github.com>
2026-05-26 23:03:58 +0530

    Add files via upload

---
+++ squareofno.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter the number is:";
+    cin>>x;
+    cout<<"Square of this no.is:";
+    cout<<x*x<<endl;
+    cout<<"enter the second number is:";
+    cin>>y;
+    cout<<"Cube of this numbers is:";
+    cout<<y*y*y;
+    return 0;
+}
---
+++ sumofNum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter value of x:"<<endl;
+    cin>>x;
+    cout<<"enter the value of y:"<<endl;
+    cin>>y;
+    cout<<x*y;
+    return 0;
+}
